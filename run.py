60646d660340aa80eff06acf6e31252de92fa427
antonio <antonio@gmail.com>
2025-09-26 17:56:06 +0000

    cambios locales

--- run.py
+++ run.py
@@ -6,4 +6,5 @@ if __name__ == "__main__":
     print(app.url_map)
     for rule in app.url_map.iter_rules():
         print(rule)
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000,debug=True)
